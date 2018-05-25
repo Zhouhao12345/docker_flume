411c0a30ef2c673f7e1e56c2ab23dba8dffa8fa5
Zhouhao12345 <zhouhao19931002@163.com>
2018-05-25 16:16:03 +0800

    [FIX]bug

--- container_log_sed.sh
+++ container_log_sed.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 log_path=$(docker inspect --format='{{.LogPath}}' $1)
-sed "s?host_log?$log_path?g" docker-compose.yml.template >> docker_compose.yml
+sed "s?host_log?$log_path?g" docker-compose.yml.template >> docker-compose.yml
